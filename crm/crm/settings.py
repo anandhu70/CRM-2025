dd385b7afc636bf02501d73e2b28ef70d612f6f1
anandhu <sreekuttenanandhu@gmail.com>
2025-10-06 13:36:57 +0530

    initial

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles','students','authentication'
+    'django.contrib.staticfiles','students','authentication','course'
 ]
 
 MIDDLEWARE = [
